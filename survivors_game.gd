0c3c0656e1ff1aa7ab93b26a9689689cfbce837a
Celine <hewong4@s.sfusd.edu>
2024-05-28 14:15:13 -0700

    background music

--- survivors_game.gd
+++ survivors_game.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+@onready var level_timer_label = %LevelTimerLabel
+
+var level_timer = 0.0
+var start_level_msec = 0.0
+
+func _ready():
+	start_level_msec = Time.get_ticks_msec()
+
+func _process(delta):
+	level_timer = Time.get_ticks_msec() - start_level_msec
+	level_timer_label.text = str(level_timer / 1000.0)
+
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
